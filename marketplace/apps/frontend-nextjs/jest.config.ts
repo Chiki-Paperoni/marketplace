a01cd478fac967c5870d8f145cb1a6a28e7ca0f6
Eugene <tv81eugene@gmail.com>
2026-02-13 18:39:50 +0200

    feat: next js project init

---
+++ marketplace/apps/frontend-nextjs/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  displayName: 'frontend-nextjs',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/frontend-nextjs',
+  testEnvironment: 'jsdom',
+};
+
+export default createJestConfig(config);

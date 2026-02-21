d9af2cf484abfaee1eb080d74669891c4e0dfd91
Eugene <tv81eugene@gmail.com>
2026-02-21 19:31:32 +0200

    feat: swagger interface added

--- marketplace/apps/backend/src/auth/dto/login.dto.ts
+++ marketplace/apps/backend/src/auth/dto/login.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 /**
@@ -6,9 +7,17 @@ import { IsEmail, IsString, MinLength } from 'class-validator';
  */
 export class LoginDto {
   @IsEmail()
+  @ApiProperty({
+    description: 'The email of the user',
+    example: 'test@example.com',
+  })
   email!: string;
 
   @IsString()
+  @ApiProperty({
+    description: 'The password of the user',
+    example: 'password',
+  })
   @MinLength(1, { message: 'Password is required' })
   password!: string;
 }
--- marketplace/apps/backend/src/main.ts
+++ marketplace/apps/backend/src/main.ts
@@ -3,7 +3,7 @@
  * This is only a minimal backend to get started.
  */
 import 'dotenv/config';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
@@ -19,10 +19,41 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const config = new DocumentBuilder()
+  .setTitle('Marketplace API')
+  .setDescription('Marketplace API description')
+  .setVersion('1.0')
+  .addTag('marketplace')
+  .addBearerAuth(
+    {
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'Authorization',
+      in: 'header',
+    },
+    'access-token',
+  )
+  .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    jsonDocumentUrl: '/swagger/json',
+    // customCssUrl: '/swagger-static/swagger-ui.css',
+    // customJs: [
+    //   '/swagger-static/swagger-ui-bundle.js',
+    //   '/swagger-static/swagger-ui-standalone-preset.js',
+    // ],
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+  Logger.log(
+    `🚀 Swagger documentation is running on: http://localhost:${port}/swagger`
   );
 }
 
